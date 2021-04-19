dc71d87d936077fcfe23dd68df6f72acd005fbdc
susanqisun <46974108+susanqisun@users.noreply.github.com>
2021-04-19 13:09:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+import os
+...
+port = int(os.environ.get('PORT', 5000))
+...
+app.run(host='0.0.0.0', port=port, debug=True)
+
 setup(
     name='flasky',
     packages=['flaskr'],
